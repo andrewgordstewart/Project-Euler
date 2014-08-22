49dcf007d3f9e036234d50dcab9f226151261553
kaptainkayak <kaptainkayak@gmail.com>
2014-08-21 23:14:16 -0230

    added lots to factor.py, solved problem 23

---
+++ bin/problem23.py
@@ -0,0 +1,17 @@
+from proj_eul.prime_tools.factor import is_abundant as _is_abundant
+
+n = 28123
+abundant_numbers = []
+for i in range(1, n):
+    if _is_abundant(i):
+        abundant_numbers.append(i)
+
+print len(abundant_numbers)
+
+abundant_sums = set()
+for i in abundant_numbers:
+    for j in abundant_numbers:
+        abundant_sums.add(i+j)
+
+x = sum(i for i in set(range(1,n)) - abundant_sums)
+print x
